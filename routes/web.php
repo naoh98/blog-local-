aa50d9ca7e22fcdb721c9b5dc1a51d7c1da3ba51
Hoan Nguyen <hoanfithou@gmail.com>
2021-07-31 03:10:09 +0700

    done

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\URL;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +13,9 @@ use Illuminate\Support\Facades\Auth;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+if (env('APP_ENV') === 'blogmotion') {
+    URL::forceSchema('https');
+}
 //------------------------------------------FRONTEND---------------------------------------------//
 //-----------------------------------------------------------------------------------------------//
 //view trang bìa
